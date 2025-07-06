49b76b6c676b58db6a15f9e7a4ad6b64a7fa5bba
ShuyiSteve <shuyi1120@outlook.com>
2025-07-06 22:31:49 +0800

    Site updated: 2025-07-06 22:31:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/23/hello-world/"];function toRandomPost(){
+var posts=["2025/06/23/hello-world/","2025/07/06/常见的排序算法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
