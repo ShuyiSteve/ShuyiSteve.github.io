e5357e50077a99bdc3046178cc378818b09ec540
ShuyiSteve <shuyi1120@outlook.com>
2025-07-24 02:04:44 +0800

    Site updated: 2025-07-24 02:04:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/23/hello-world/","2025/07/06/常见的排序算法/"];function toRandomPost(){
+var posts=["2025/07/24/VUE3初识/","2025/06/23/hello-world/","2025/07/06/常见的排序算法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
